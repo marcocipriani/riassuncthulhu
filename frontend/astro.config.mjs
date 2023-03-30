c0fe421cbc95c010795bd39c2886965313d1700d
marcocipriani <marcocipriani@users.noreply.github.com>
2023-03-30 12:23:37 +0200

    connect with sanity

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from "astro-sanity";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		sanity({
+			projectId: '5wf3w7bm', //import.meta.env.SANITY_PROJECT_ID
+			dataset: 'production', //import.meta.env.SANITY_DATASET
+			apiVersion: '2023-02-08', //import.meta.env.SANITY_API_VERSION
+			useCdn: false,
+		}),
+	],
+});
